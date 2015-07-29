5cc60a39147a585f0355e936a198fe95aa0d8dd2
gingsum <gingsum@gmail.com>
2015-07-29 14:54:34 -0700

    fixed homepage, about me, adjusted positions

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
@@ -11,9 +10,16 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/homepage');
+});
+app.get('/3x3_TTT', function(request, response) {
+  response.render('pages/3x3_TTT');
+});
+app.get('/about', function(request, response) {
   response.render('pages/about');
 });
-
+app.get('/9x9_TTT', function(request, response) {
+  response.render('pages/9x9_TTT');
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
